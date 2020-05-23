51b5ba7ce0fbc6da50b590e749faeceaaa3d0660
Dante Hatcher <dhatcher@uoregon.edu>
2020-05-22 23:21:17 -0700

    Edited to-do.html and to-do.js files to create a simple to-do list web application.

--- client/javascripts/to-do.js
+++ client/javascripts/to-do.js
@@ -1,7 +1,6 @@
 //jshint esversion: 6
 
 let controller = function() {
-
   let addCommentFromInputBox = function() {
     //Semmy uses "$" to name variables that will contain jQuery objects
     let $new_comment;
@@ -12,8 +11,19 @@ let controller = function() {
       $(".comments").append($new_comment);
       //$new_comment.fadeIn();
       $(".comment-input input").val("");
+
+      //log the list of aragraph elements
+      console.log($(".comments").html());
     }
   };
+
+  if (localStorage.getItem("toDoList")) {
+    localStorage.setItem("toDoList", $(".comments").html());
+
+    $(".comments").html(localStorage.getItem("toDoList"));
+  }
+
+  // ^^ .documents.pagelistener?? ^
 
   $(".comment-input button").on("click", function(event) {
     addCommentFromInputBox();
